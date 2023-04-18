3e615ddb7c303935c52d9e84811dd87c18faccbd
eayazuddin <91460445+eayazuddin@users.noreply.github.com>
2023-04-18 09:17:45 +0600

    Update practice1.dart

--- practice1.dart
+++ practice1.dart
@@ -1,1 +1,20 @@
 
+/*Create a map (key value pair) of an employee, * the keys will be employee = [Id, Name, Age, Salary, Gender].
+In this map [Id, Age, Salary] only these three keys' values
+will be in number.
+At last print out the Salary from the map
+*/
+
+void main() {
+  
+  
+Map<String, dynamic> employee = {
+  'Id': 1231234,
+  'Name': 'Md. Shakil Hasan',
+  'Age': 23,
+  'Salary': 45000,
+  'Gender': 'Male'
+  }; 
+  print(employee['Salary']);
+
+}
