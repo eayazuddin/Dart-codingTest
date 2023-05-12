5e1bf32901759b95b41e5c4f66411b4cb32b4cc7
eayazuddin <91460445+eayazuddin@users.noreply.github.com>
2023-05-12 06:40:43 +0600

    Update practice2.dart

--- practice2.dart
+++ practice2.dart
@@ -4,21 +4,27 @@ Write a Dart program that uses this function to take input from the user, and pr
 For example, if the user inputs the list [2, 5, 8, 11, 13, 18, 21, 24], the program should return the output list [5, 11, 13, 21]. 
 Hint: You can use an if statement to check if a number is odd or even.
 */
-void main()
-{
-   List<int> numbers = [2, 5, 8, 11, 13, 18, 21, 24];
-  List<int> oddNumbers = getOddNumbers(numbers); 
-  print(oddNumbers);
+
+void main() {
+  // Get the input list from the user
+  List<int> numbers = [2, 5, 8, 11, 13, 18, 21, 24];
+
+  // Get the odd numbers from the input list
+  List<int> oddNumbers = getOddNumbers(numbers);
+
+  // Print the input list and the output list
+  print("Input list: $numbers");
+  print("Output list: $oddNumbers");
 }
-List<int> getOddNumbers(List<int> numbers)
-{ 
-  List<int> oddNumbers =[];
-  for(int x in numbers)
-  {
-    if(x % 2 != 0)
-    {
-      oddNumbers.add(x);
+
+List<int> getOddNumbers(List<int> numbers) {
+  List<int> oddNumbers = [];
+  for (int number in numbers) {
+    if (number % 2 != 0) {
+      oddNumbers.add(number);
     }
   }
-  return oddNumbers; 
+  return oddNumbers;
 }
+
+
